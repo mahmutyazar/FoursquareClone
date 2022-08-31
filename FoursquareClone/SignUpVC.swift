a3fc6ffa1590398d4d4a955381f780aa701b8930
Mahmut Yazar <yazarmahmut34@gmail.com>
2022-08-31 18:53:24 +0300

    First Commit

---
+++ FoursquareClone/SignUpVC.swift
@@ -0,0 +1,79 @@
+//
+//  ViewController.swift
+//  FoursquareClone
+//
+//  Created by Mahmut Yazar on 1.06.2022.
+//
+
+import UIKit
+import Parse
+
+class SignUpVC: UIViewController {
+    @IBOutlet weak var userNameText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    
+    @IBOutlet weak var headerText: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    
+        
+        
+    }
+    
+    @IBAction func signInClicked(_ sender: Any) {
+        
+        if userNameText.text != "" && passwordText.text != "" {
+            
+            PFUser.logInWithUsername(inBackground: userNameText.text!, password: passwordText.text!) { (user, error) in
+                if error != nil {
+                    self.makeAlert(titleInput: "Error!", messageInput: error?.localizedDescription ?? "Error!")
+                } else {
+                    
+                    //Segue
+                    self.performSegue(withIdentifier: "toPlacesVC", sender: nil)
+                    
+                }
+            }
+            
+            
+        } else {
+            makeAlert(titleInput: "Error!", messageInput: "Username / Password?")
+        }
+        
+        
+    }
+    
+    @IBAction func signUpClicked(_ sender: Any) {
+        
+        if userNameText.text != "" && passwordText.text != "" {
+            
+            let user = PFUser()
+            user.username = userNameText.text!
+            user.password = passwordText.text!
+            
+            user.signUpInBackground { success, error in
+                if error != nil {
+                    self.makeAlert(titleInput: "Error!", messageInput: error?.localizedDescription ?? "Error")
+                } else {
+                    
+                    self.performSegue(withIdentifier: "toPlacesVC", sender: nil)
+                    
+                }
+            }
+        } else {
+            makeAlert(titleInput: "Error!", messageInput: "Username/Password?")
+        }
+        
+        
+        }
+    func makeAlert(titleInput: String, messageInput: String) {
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
+        
+    }
+}
+
